1ec2a77337dda708e546968c895bd03d40e15941
Adrian Fota <adrian.mihael.fota@gmail.com>
2025-12-28 10:10:04 +0200

    Add refractory period and improve logic

--- lif.jl
+++ lif.jl
@@ -4,28 +4,40 @@ gr()
 
 mutable struct Neuron
     τ_m::Float64 # Time constant
+    τ_ref::Float64 # Refractory period
     V_rest::Float64 # Resting potential
     V_thresh::Float64 # Threshold potential
     V_reset::Float64 # Reset potential
     R_m::Float64 # Resistance
     v::Float64 # Current membrane potential
+    t_ref::Float64 # Current remaining refractory time
 end
 
-function Neuron(; τ_m=10.0, V_rest=-75.0, V_thresh=-55.0, 
+function Neuron(; τ_m=10.0, τ_ref=2.0, V_rest=-75.0, V_thresh=-55.0, 
                 V_reset=-75.0, R_m=1.0)
-    return Neuron(τ_m, V_rest, V_thresh, V_reset, R_m, V_rest)
+    return Neuron(τ_m, τ_ref, V_rest, V_thresh, V_reset, R_m, V_rest, 0.0)
 end
 
 function update!(n::Neuron, I_ext::Float64, dt::Float64)
+    # If in refractory period, voltage stuck at V_ref
+    if n.t_ref > 0
+        n.v = n.V_reset
+        n.t_ref -= dt
+        return false
+    end
+
+    # LIF Equation
     dv = (-(n.v - n.V_rest) + n.R_m * I_ext) / n.τ_m * dt
     n.v += dv
 
+    # Spike check
     if n.v ≥ n.V_thresh
         n.v = n.V_reset
+        n.t_ref = n.τ_ref
         return true
-    else
-        return false
     end
+
+    return false
 end
 
 n = Neuron()
@@ -37,11 +49,7 @@ volts = Float64[]
 for t in times
     I_ext = t > 10.0 ? 25.0 : 0.0 # Stimulation starts after 10ms
     fired = update!(n, I_ext, dt)
-    if fired
-        push!(volts, 20.0)
-    else
-        push!(volts, n.v)
-    end
+    push!(volts, n.v)
 end
 
 p = plot(times, volts, 
