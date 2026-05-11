9ca994467d05f3f3ff3d375c02fddd36aa42db6e
Adrian Fota <adrian.mihael.fota@gmail.com>
2026-05-11 23:10:45 +0300

    Fix name typo

--- examples/minisnn_2L_50N_ECG.jl
+++ examples/minisnn_2L_50N_ECG.jl
@@ -27,7 +27,7 @@ output_template = Neuron("output"; R_m=3.0, τ_m=6.0, τ_s=6.0, τ_ref=2.0,
                 τ_pretrace=20.0, τ_posttrace=10.0)
 
 input_layer =  NeuronLayer(n_neurons, input_template; name="input", V_thresh_dev=0.05, R_m_dev=0.1, τ_m_dev=0.15)
-output_layer =  NeuronLayer(n_neurons, output_template; name="input", V_thresh_dev=0.05, R_m_dev=0.1, τ_m_dev=0.15)
+output_layer =  NeuronLayer(n_neurons, output_template; name="output", V_thresh_dev=0.05, R_m_dev=0.1, τ_m_dev=0.15)
 
 synapse_template = Synapse(1, 2; learningrate=0.05, w=0.5, wmax=1.0)
 synapse_layer = SynapseLayer(input_layer, output_layer, synapse_template; dist=NormalDist(0.5, 0.2), density=0.75, pre_idx=1, post_idx=2)
